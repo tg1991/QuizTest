971a1275caa53239f3be57354de56f5000a1970b
tg1991 <36793219+tg1991@users.noreply.github.com>
2020-12-06 23:20:18 +0000

    Development (#1)
    * adding in buttons
    
    * changes

---
+++ QuizApp/ClientApp/README.md
@@ -0,0 +1,6 @@
+React asp.net core app to insert questions and take the quiz once questions answered.
+
+Only did the insert questions not the quiz part, can discuss why on the call.
+
+Thank you 
+Tom 
---
+++ QuizApp/DataGenerator.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace QuizApp
+{
+    public class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new QuizDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<QuizDbContext>>()))
+            {
+                // Look for any board games.
+                if (context.Questions.Any())
+                {
+                    return;   // Data was already seeded
+                }
+
+                context.Questions.AddRange(
+                    new Models.Question
+                    {
+                        ID = 1,
+                        QuestionText = "What year was the Queens Coronation?",
+                        A = "1963",
+                        B = "1951",
+                        C = "1953",
+                        D = "1961",
+                        CorrectAnswer = "C"
+                    },
+                    new Models.Question
+                    {
+                        ID = 2,
+                        QuestionText = "What first name was Voldemort born with?",
+                        A = "Tom",
+                        B = "Quincy",
+                        C = "Charles",
+                        D = "Joe",
+                        CorrectAnswer = "A"
+                    });
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
